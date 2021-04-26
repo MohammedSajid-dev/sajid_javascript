0712af66031f5f88c32530595168d25782168fed
MohammedSajid-Dev <iquamsajid@live.com>
2021-04-26 23:43:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+    // console.log("Welcome");
+    // console.error("This is error message")
+    // console.warn("This is a warning");
+    // document.write("<h1>Welcome</h1>");
+    // var name="Sajid";
+    // let age=31;
+    // document.write(name);
+    // document.write("<br>");
+    // document.write(age);
+    // document.write("<br>");
+    // document.write(typeof (name));
+    // document.write("<br>");
+    // document.write(typeof age);
+    
+    // const pi=3.15;
+    // document.write(pi);
+
+// var student={
+//     name: "sajid",
+//     age:31,
+//     isEligble:true
+// }
+// document.write(student.name);
+
+// var months=["Jan","Feb", "March"];
+// document.write(months[0]);
+
+// function welcome(){ //function defination
+//     document.write("welsome");
+// }
+// welcome(); //function calling
+
+// function sum(x,y){
+// let z=x+y;
+// document.write("sum = "+z);
+// }
+// sum(50,50);
+
+// function display(){
+//         alert("Success");
+// }
+// display();
+
+// function welcome(name){ //where name is called as parameter.
+//     document.write("welcome ="+name);
+// }
+// welcome("SAJJU"); //where SAJJU is the argument.
+
+// let a=10;
+// let b=10;
+// // Addition Operator
+// let add= a+b;
+// console.log(add);
+// // SubractionOperator
+// let sub= a-b;
+// console.log(sub);
+// // Multiply Operator
+// let mul= a*b;
+// console.log(mul);
+// // Division operator
+// let div= a/b;
+// console.log(div);
+// // Reminder/Modulas operator
+// let mod=a%b;
+// console.log(mod);
+// // Increment & Decrement Operator
+// a++;
+// console.log(a);
+// b--;
+// console.log(b);
+
+// if(10>20){
+// console.log("10 is lesser");
+// }
+// else{
+//     console.log("5 is greater");
+// }
+
+let i=1;
+do{
+console.log(i);
+i++;
+}while(i<=5);
